5a6d24c74f1b41795b23bb6f0143f4fc37201fc4
Nikita-Vesel0v <nikiveselov2004@gmail.com>
2024-05-11 10:34:38 +0300

    Add createBattleField function for user input of Xs and Os sequence and display the filled game board

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,35 @@
 import kotlin.random.Random
 
 fun main() {
-    battleField()
+    randomBattleField()
+    createBattleField()
 }
 
-fun battleField() {
+fun randomBattleField() {
     val field = List(3) {
         Array(3) { if (Random.nextBoolean()) "X" else "O" }
     }
-
+    println("It's random battle field")
     field.forEach{ println(it.joinToString(" ")) }
 
+}
+
+fun createBattleField() {
+    println("\nTo play, please enter sequence of 9: \"X\", \"0\" or \"_\"")
+    val sequence = readln().split("").toMutableList()
+    sequence.removeFirst()
+    sequence.removeLast()
+    val field = listOf(
+        sequence.subList(0,3), sequence.subList(3,6), sequence.subList(6,9)
+    )
+    println(
+        """
+        It's battle field"
+        ---------
+        | ${field[0].joinToString(" ")} |
+        | ${field[1].joinToString(" ")} |
+        | ${field[2].joinToString(" ")} |
+        ---------
+        """.trimIndent()
+    )
 }
