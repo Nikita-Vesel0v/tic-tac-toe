44371f5a25e0f59f9574c121ea4b9727b6e10241
Nikita-Vesel0v <nikiveselov2004@gmail.com>
2024-05-12 21:43:09 +0300

    Users can play together

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,37 +4,40 @@ import kotlin.random.Random
 fun main() {
     var field = randomBattleField()
     printField(field)
-    val sequence = readSequence()
-    field = createBattleField(sequence)
+    val emptyField = List(9) { "_" }
+//    val sequence = readSequence()
+    field = createBattleField(emptyField)
     printField(field)
     var resultPlay = checkWinner(field)
     println(resultPlay)
-    if (resultPlay == "Game not finished") {
-        field = makeMove(field)
+    var xOrO = 9
+    while (resultPlay == "Game not finished") {
+        field = makeMove(field, (xOrO % 2 == 1))
         printField(field)
         resultPlay = checkWinner(field)
         println(resultPlay)
+        xOrO --
     }
 }
 
-fun randomBattleField(): List<MutableList<String>> =
+fun randomBattleField(): List<List<String>> =
     List(3) {
-        MutableList(3) { if (Random.nextBoolean()) "X" else "O" }
+        List(3) { if (Random.nextBoolean()) "X" else "O" }
     }
 
-fun createBattleField(sequence: MutableList<String>): List<MutableList<String>> =
+fun createBattleField(sequence: List<String>): List<List<String>> =
      listOf (
         sequence.subList(0, 3),
         sequence.subList(3, 6),
         sequence.subList(6, 9)
     )
 
-fun checkWinner(field: List<MutableList<String>>): String {
+fun checkWinner(field: List<List<String>>): String {
     fun countOfElement(value: String): Int {
         var sum = 0; field.forEach { sum += it.count { valueElem -> valueElem == value } }
         return sum
     }
-    fun isWin (field: List<MutableList<String>>, value: String): Boolean =
+    fun isWin (field: List<List<String>>, value: String): Boolean =
         (field[0][0] == value && ((field[1][0] == value && field[2][0] == value) || (field[0][1] == value && field[0][2] == value) || (field[1][1] == value && field[2][2] == value))) ||
                 (field[0][1] == value && (field[1][1] == value && field[2][1] == value)) ||
                 (field[0][2] == value && (field[1][2] == value && field[2][2] == value || (field[1][1] == value && field[2][0] == value))) ||
@@ -56,7 +59,8 @@ fun checkWinner(field: List<MutableList<String>>): String {
     }
 }
 
-fun makeMove(field: List<MutableList<String>>): List<MutableList<String>> {
+fun makeMove(field: List<List<String>>, isX: Boolean): List<MutableList<String>> {
+    val newField = field.map { it.toMutableList() }
     while(true) {
         try {
             val (i, j) = readln().split(" ").map { it.toInt() }
@@ -64,29 +68,33 @@ fun makeMove(field: List<MutableList<String>>): List<MutableList<String>> {
                 i > 3 || j > 3 || i < 1 || j < 1 -> println("Coordinates should be from 1 to 3!")
                 field[i - 1][j - 1] == "O" || field[i - 1][j - 1] == "X" -> println("This cell is occupied! Choose another one!")
                 else -> {
-                    field[i - 1][j - 1] = "X"
+                    newField[i - 1][j - 1] = if (isX) "X" else "O"
                     break
                 }
-
             }
         } catch (e: Exception) {
             println("You should enter numbers!")
         }
     }
-    return field
+    return newField
 }
 
-fun printField(field: List<MutableList<String>>) {
+fun printField(field: List<List<String>>) {
+    val fieldSpace = List(3) { MutableList(3) {" "} }
+
     for(i in field.indices) {
-        field[i].forEach { if (it == "_") field[i][field[i].indexOf(it)] = " "}
+        field[i].forEach {
+            if (it != "_") fieldSpace[i][field[i].indexOf(it)] = it
+        }
+
     }
     println(
         """
         It's battle of field
         ---------
-        | ${field[0].joinToString(" ")} |
-        | ${field[1].joinToString(" ")} |
-        | ${field[2].joinToString(" ")} |
+        | ${fieldSpace[0].joinToString(" ")} |
+        | ${fieldSpace[1].joinToString(" ")} |
+        | ${fieldSpace[2].joinToString(" ")} |
         ---------
         """.trimIndent()
     )
